cf16afc002403328c88b15cee2859afae5127c71
Karel Cemus <karel.cemus@gmail.com>
2015-11-04 12:04:37 +0100

    #11 Dropped Play plugins as they are deprecated

--- src/main/scala/play/cache/api/CachePlugin.scala
+++
@@ -1,9 +0,0 @@
-package play.cache.api
-
-import play.api.Plugin
-
-trait CachePlugin extends Plugin {
-
-  /** Implementation of the the Cache plugin provided by this plugin. */
-  def api: CacheAPI
-}
--- src/main/scala/play/cache/api/CachePlugin20.scala
+++
@@ -1,9 +0,0 @@
-package play.cache.api
-
-import play.api.Plugin
-
-trait CachePlugin20 extends Plugin {
-
-  /** Implementation of the the Cache plugin provided by this plugin. */
-  def api: CacheAPI20
-}
