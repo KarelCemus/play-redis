a60153f96950438e648add0ae08e35fdffb3f0ff
Karel Cemus <karel.cemus@gmail.com>
2015-11-04 12:43:36 +0100

    #11 Refactored BrandoSpec to be more readable

--- src/test/scala/play/cache/BrandoSpec.scala
+++ src/test/scala/play/cache/BrandoSpec.scala
@@ -2,10 +2,6 @@ package play.cache
 
 import java.util.concurrent.TimeUnit
 
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-
 import play.api.libs.concurrent.Akka
 
 import akka.actor.ActorRef
@@ -30,64 +26,70 @@ class BrandoSpec extends Specification with RedisCacheSupport {
     implicit val timeout = Timeout( 1000, TimeUnit.MILLISECONDS )
 
     "ping" in {
-      redis ?> Request( "PING" ) must beEqualTo( Pong )
+      redis ? Request( "PING" ) expects Value must beEqualTo( Pong )
     }
 
     "set value" in {
-      redis ?> Request( "SET", "some-key", "this-value" ) must beEqualTo( Ok )
+      redis ? Request( "SET", "some-key", "this-value" ) expects Value must beEqualTo( Ok )
     }
 
     "get stored value" in {
-      redis ?>> Request( "GET", "some-key" ) must beEqualTo( "this-value" )
+      redis ? Request( "GET", "some-key" ) expects String must beEqualTo( "this-value" )
     }
 
     "get non-existing value" in {
-      redis ? Request( "GET", "non-existing" ) must beNone
+      redis ? Request( "GET", "non-existing" ) expects Option must beNone
     }
 
     "determine whether it contains already stored key" in {
-      redis ?> Request( "EXISTS", "some-key" ) must beEqualTo( 1L )
+      redis ? Request( "EXISTS", "some-key" ) expects Value must beEqualTo( 1L )
     }
 
     "determine whether it contains non-existent key" in {
-      redis ?> Request( "EXISTS", "non-existing" ) must beEqualTo( 0L )
+      redis ? Request( "EXISTS", "non-existing" ) expects Value must beEqualTo( 0L )
     }
 
     "delete stored value" in {
-      redis ?> Request( "DEL", "some-key" ) must beEqualTo( 1L )
+      redis ? Request( "DEL", "some-key" ) expects Value must beEqualTo( 1L )
     }
 
     "delete already deleted value" in {
-      redis ?> Request( "DEL", "some-key" ) must beEqualTo( 0L )
+      redis ? Request( "DEL", "some-key" ) expects Value must beEqualTo( 0L )
     }
 
     "delete non-existing value" in {
-      redis ?> Request( "DEL", "non-existing" ) must beEqualTo( 0L )
+      redis ? Request( "DEL", "non-existing" ) expects Value must beEqualTo( 0L )
     }
 
     "determine whether it contains deleted key" in {
-      redis ?> Request( "EXISTS", "some-key" ) must beEqualTo( 0L )
+      redis ? Request( "EXISTS", "some-key" ) expects Value must beEqualTo( 0L )
     }
 
     "flush current database" in {
-      redis ?> Request( "FLUSHDB" ) must beEqualTo( Ok )
+      redis ? Request( "FLUSHDB" ) expects Value must beEqualTo( Ok )
     }
 
     "determine whether it contains key after invalidation" in {
-      redis ?> Request( "EXISTS", "model" ) must beEqualTo( 0L )
+      redis ? Request( "EXISTS", "model" ) expects Value must beEqualTo( 0L )
     }
   }
 
-  implicit class BrandoRequest( brando: ActorRef ) {
+  sealed trait Expectation
+  trait StringExpectation extends Expectation
+  trait ValueExpectation extends Expectation
+  trait OptionExpectation extends Expectation
 
-    def ?( request: Request )( implicit timeout: Timeout ) = {
-      val response = new AskableActorRef( brando ).ask( request ).map( _.asInstanceOf[ Option[ Any ] ] )
-      Await.result( response, Duration( "5s" ) )
-    }
+  object String extends StringExpectation
+  object Value extends ValueExpectation
+  object Option extends OptionExpectation
 
-    def ?>( request: Request )( implicit timeout: Timeout ) = ?( request ).getOrElse( None )
-
-    def ?>>( request: Request )( implicit timeout: Timeout ) = Response.AsString.unapply( ?( request ) ).getOrElse( None )
+  implicit class ExpectationTransformer( value: Option[ Any ] ) {
+    def expects( expectation: StringExpectation ) = Response.AsString.unapply( value ).orNull
+    def expects( expectation: ValueExpectation ) = value.orNull
+    def expects( expectation: OptionExpectation ) = value
   }
 
+  implicit class BrandoRequest( brando: ActorRef )( implicit timeout: Timeout ) {
+    def ?( request: Request ) = new AskableActorRef( brando ).ask( request ).sync.asInstanceOf[ Option[ Any ] ]
+  }
 }
--- src/test/scala/play/cache/RedisCacheSupport.scala
+++ src/test/scala/play/cache/RedisCacheSupport.scala
@@ -6,57 +6,58 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 //import scala.language.implicitConversions
 
-import play.api.cache.{CacheApi=>Api}
-import play.api.inject._
-import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.{Application, Play}
+import play.api.Application
+import play.api.cache.{CacheApi => Api}
 import play.api.test.{FakeApplication, PlayRunners}
-import play.cache.redis._
 
 import org.specs2.matcher._
 import org.specs2.mutable.Specification
-import org.specs2.specification.core.Fragments
 import org.specs2.specification.Step
+import org.specs2.specification.core.Fragments
 
 /**
  * Takes care of redis connection and play application
  */
-trait RedisCacheSupport {
-  self: Specification =>
+trait RedisCacheSupport { self: Specification =>
 
-  protected val invoke = this
+//  protected val invoke = this
 
   /** application context to perform operations in */
   protected implicit def application: Application = FakeApplication()
 
   def start( ): Unit = PlayRunners.mutex.synchronized {
-    // start play application
+//    // start play application
 //    if ( Running.counter.incrementAndGet( ) == 1 ) {
 //      println("-------- starting ------")
 //      Play.start( application )
-      // reload cache in case the play application was stopped
+//      // reload cache in case the play application was stopped
 //      play.cache.AsyncCache.reload( )
-      // invalidate redis cache for test
+//      // invalidate redis cache for test
 //      application.injector.instanceOf[ CacheAPI20 ].invalidate()
 //      invoke inFuture play.cache.AsyncCache.invalidate( )
 //    }
   }
 
   def stop( ): Unit = PlayRunners.mutex.synchronized {
-    // stop play application
+//    // stop play application
 //    if ( Running.counter.decrementAndGet( ) == 0 ) Play.stop( application )
   }
+//
+//  override def map( fs: => Fragments ): Fragments = Step( start( ) ) ^ fs ^ Step( stop( ) )
+//
+//  implicit def inFuture[ T ]( value: Future[ T ] ): T = Await.result( value, Duration( "5s" ) )
+//
+//  implicit def matcher[ T ]( matcher: Matcher[ T ] ): Matcher[ Future[ T ] ] = new Matcher[ Future[ T ] ] {
+//    override def apply[ S <: Future[ T ] ]( value: Expectable[ S ] ): MatchResult[ S ] = {
+//      val expectable: Expectable[ Future[ T ] ] = value
+//      val matched = expectable.map( inFuture[ T ] _ ).applyMatcher( matcher )
+//      result( matched, value )
+//    }
+//  }
 
-  override def map( fs: => Fragments ): Fragments = Step( start( ) ) ^ fs ^ Step( stop( ) )
-
-  implicit def inFuture[ T ]( value: Future[ T ] ): T = Await.result( value, Duration( "5s" ) )
-
-  implicit def matcher[ T ]( matcher: Matcher[ T ] ): Matcher[ Future[ T ] ] = new Matcher[ Future[ T ] ] {
-    override def apply[ S <: Future[ T ] ]( value: Expectable[ S ] ): MatchResult[ S ] = {
-      val expectable: Expectable[ Future[ T ] ] = value
-      val matched = expectable.map( inFuture[ T ] _ ).applyMatcher( matcher )
-      result( matched, value )
-    }
+  /** waits for future responses and returns them synchronously */
+  implicit class Synchronizer[ T ]( future: Future[ T ] ) {
+    def sync = Await.result( future, Duration( "5s" ) )
   }
 }
 
