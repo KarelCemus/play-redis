6f6c6b72b730b53ca946f363e8351da83eead557
Karel Cemus <karel.cemus@gmail.com>
2016-03-29 09:54:16 +0200

    Simplified Builders

--- src/main/scala/play/api/cache/redis/Builders.scala
+++ src/main/scala/play/api/cache/redis/Builders.scala
@@ -13,45 +13,19 @@ object Builders {
   type Identity[ A ] = A
   type Future[ A ] = scala.concurrent.Future[ A ]
 
-  trait ResultBuilder[ Result[ _ ] ] {
-    self =>
+  trait ResultBuilder[ Result[ X ] ] {
     /** converts future result produced by Redis to the result of desired type */
     def toResult[ T ]( value: Future[ T ] )( implicit context: ExecutionContext, timeout: akka.util.Timeout ): Result[ T ]
-
-    /** chains calls and passes the output of the first as the input of the subsequent */
-    def andThen[ A, B ]( source: => Result[ A ] )( andThen: A => Result[ B ] )( implicit context: ExecutionContext ): Result[ B ]
-
-    /** chains calls and passes the output of the first as the input of the subsequent expecting the future result */
-    def andFuture[ A, B ]( source: => Result[ A ] )( andThen: A => Future[ B ] )( implicit context: ExecutionContext ): Future[ B ]
-
-    /** Chain results */
-    implicit class ResultChainer[ A ]( source: => Result[ A ] ) {
-
-      /** chains calls and passes the output of the first as the input of the subsequent */
-      def andThen[ B ]( andThen: A => Result[ B ] )( implicit context: ExecutionContext ) = self.andThen( source )( andThen )
-
-      /** chains calls and passes the output of the first as the input of the subsequent expecting the future result */
-      def andFuture[ B ]( andThen: A => Future[ B ] )( implicit context: ExecutionContext ) = self.andFuture( source )( andThen )
-    }
-
   }
 
   /** returns the future itself without any transformation */
   object AsynchronousBuilder extends ResultBuilder[ Future ] {
     override def toResult[ T ]( future: Future[ T ] )( implicit context: ExecutionContext, timeout: akka.util.Timeout ): Future[ T ] = future
-
-    override def andThen[ A, B ]( source: => Future[ A ] )( andThen: ( A ) => Future[ B ] )( implicit context: ExecutionContext ) = source.flatMap( andThen )
-
-    override def andFuture[ A, B ]( source: => Future[ A ] )( andThen: ( A ) => Future[ B ] )( implicit context: ExecutionContext ) = source.flatMap( andThen )
   }
 
   /** converts the future into the value */
   object SynchronousBuilder extends ResultBuilder[ Identity ] with Implicits {
     override def toResult[ T ]( future: Future[ T ] )( implicit context: ExecutionContext, timeout: akka.util.Timeout ): Identity[ T ] = future.sync( timeout.duration )
-
-    override def andThen[ A, B ]( source: => Identity[ A ] )( andThen: ( A ) => Identity[ B ] )( implicit context: ExecutionContext ) = andThen( source )
-
-    override def andFuture[ A, B ]( source: => Identity[ A ] )( andThen: ( A ) => Future[ B ] )( implicit context: ExecutionContext ) = andThen( source )
   }
 
 }
