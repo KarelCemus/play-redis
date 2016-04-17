b9daf7ad1a3379ac14f2649570dc255489d9a05a
Karel Cemus <karel.cemus@gmail.com>
2016-04-17 13:19:06 +0200

    Removed Kryo dependency from the project, updated README

--- src/test/scala/play/api/cache/redis/SerializerSpecs.scala
+++ src/test/scala/play/api/cache/redis/SerializerSpecs.scala
@@ -59,18 +59,24 @@ class SerializerSpecs extends Specification {
     }
 
     "date" in {
-      new Date( 123 ).encoded mustEqual "AQBqYXZhLnV0aWwuRGF05QF7"
+      new Date( 123 ).encoded mustEqual "rO0ABXNyAA5qYXZhLnV0aWwuRGF0ZWhqgQFLWXQZAwAAeHB3CAAAAAAAAAB7eA=="
     }
 
     "datetime" in {
       new DateTime( 123456L, DateTimeZone.forID( "UTC" ) ).encoded mustEqual """
-          |AQBvcmcuam9kYS50aW1lLkRhdGVUaW3lAQEBb3JnLmpvZGEudGltZS5jaHJvbm8uSVNPQ2hyb25v
-          |bG9n+QEBAm9yZy5qb2RhLnRpbWUuY2hyb25vLkdyZWdvcmlhbkNocm9ub2xvZ/kBAAgAAICJDw==
+          |rO0ABXNyABZvcmcuam9kYS50aW1lLkRhdGVUaW1luDx4ZGpb3fkCAAB4cgAfb3JnLmpvZGEudGlt
+          |ZS5iYXNlLkJhc2VEYXRlVGltZf//+eFPXS6jAgACSgAHaU1pbGxpc0wAC2lDaHJvbm9sb2d5dAAa
+          |TG9yZy9qb2RhL3RpbWUvQ2hyb25vbG9neTt4cAAAAAAAAeJAc3IAJ29yZy5qb2RhLnRpbWUuY2hy
+          |b25vLklTT0Nocm9ub2xvZ3kkU3R1YqnIEWZxN1AnAwAAeHBzcgAfb3JnLmpvZGEudGltZS5EYXRl
+          |VGltZVpvbmUkU3R1YqYvAZp8MhrjAwAAeHB3BQADVVRDeHg=
         """.stripMargin.trim
     }
 
     "custom classes" in {
-      SimpleObject( "B", 3 ).encoded mustEqual "AQBwbGF5LmFwaS5jYWNoZS5yZWRpcy5TaW1wbGVPYmplY/QBAYJCBg=="
+      SimpleObject( "B", 3 ).encoded mustEqual  """
+        |rO0ABXNyACFwbGF5LmFwaS5jYWNoZS5yZWRpcy5TaW1wbGVPYmplY3Te6Peta/KhNAIAAkkABXZh
+        |bHVlTAADa2V5dAASTGphdmEvbGFuZy9TdHJpbmc7eHAAAAADdAABQg==
+      """.stripMargin.trim
     }
 
     "null" in {
@@ -78,7 +84,11 @@ class SerializerSpecs extends Specification {
     }
 
     "list" in {
-      List( "A", "B", "C" ).encoded mustEqual "AQBzY2FsYS5jb2xsZWN0aW9uLmltbXV0YWJsZS4kY29sb24kY29sb+4BAwMBgkEDAYJCAwGCQw=="
+      List( "A", "B", "C" ).encoded mustEqual """
+        |rO0ABXNyADJzY2FsYS5jb2xsZWN0aW9uLmltbXV0YWJsZS5MaXN0JFNlcmlhbGl6YXRpb25Qcm94
+        |eQAAAAAAAAABAwAAeHB0AAFBdAABQnQAAUNzcgAsc2NhbGEuY29sbGVjdGlvbi5pbW11dGFibGUu
+        |TGlzdFNlcmlhbGl6ZUVuZCSKXGNb91MLbQIAAHhweA==
+      """.stripMargin.trim
     }
 
   }
@@ -120,22 +130,32 @@ class SerializerSpecs extends Specification {
     }
 
     "date" in {
-      "AQBqYXZhLnV0aWwuRGF05QF7".decoded[ Date ] mustEqual new Date( 123 )
+      "rO0ABXNyAA5qYXZhLnV0aWwuRGF0ZWhqgQFLWXQZAwAAeHB3CAAAAAAAAAB7eA==".decoded[ Date ] mustEqual new Date( 123 )
     }
 
     "datetime" in {
       """
-        |AQBvcmcuam9kYS50aW1lLkRhdGVUaW3lAQEBb3JnLmpvZGEudGltZS5jaHJvbm8uSVNPQ2hyb25v
-        |bG9n+QEBAm9yZy5qb2RhLnRpbWUuY2hyb25vLkdyZWdvcmlhbkNocm9ub2xvZ/kBAAgAAICJDw==
+        |rO0ABXNyABZvcmcuam9kYS50aW1lLkRhdGVUaW1luDx4ZGpb3fkCAAB4cgAfb3JnLmpvZGEudGlt
+        |ZS5iYXNlLkJhc2VEYXRlVGltZf//+eFPXS6jAgACSgAHaU1pbGxpc0wAC2lDaHJvbm9sb2d5dAAa
+        |TG9yZy9qb2RhL3RpbWUvQ2hyb25vbG9neTt4cAAAAAAAAeJAc3IAJ29yZy5qb2RhLnRpbWUuY2hy
+        |b25vLklTT0Nocm9ub2xvZ3kkU3R1YqnIEWZxN1AnAwAAeHBzcgAfb3JnLmpvZGEudGltZS5EYXRl
+        |VGltZVpvbmUkU3R1YqYvAZp8MhrjAwAAeHB3BQADVVRDeHg=
       """.stripMargin.trim.decoded[ DateTime ] mustEqual new DateTime( 123456L, DateTimeZone.forID( "UTC" ) )
     }
 
     "custom classes" in {
-      "AQBwbGF5LmFwaS5jYWNoZS5yZWRpcy5TaW1wbGVPYmplY/QBAYJCBg==".decoded[ SimpleObject ] mustEqual SimpleObject( "B", 3 )
+      """
+        |rO0ABXNyACFwbGF5LmFwaS5jYWNoZS5yZWRpcy5TaW1wbGVPYmplY3Te6Peta/KhNAIAAkkABXZh
+        |bHVlTAADa2V5dAASTGphdmEvbGFuZy9TdHJpbmc7eHAAAAADdAABQg==
+      """.stripMargin.trim.decoded[ SimpleObject ] mustEqual SimpleObject( "B", 3 )
     }
 
     "list" in {
-      "AQBzY2FsYS5jb2xsZWN0aW9uLmltbXV0YWJsZS4kY29sb24kY29sb+4BAwMBgkEDAYJCAwGCQw==".decoded[ List[ String ] ] mustEqual List( "A", "B", "C" )
+      """
+        |rO0ABXNyADJzY2FsYS5jb2xsZWN0aW9uLmltbXV0YWJsZS5MaXN0JFNlcmlhbGl6YXRpb25Qcm94
+        |eQAAAAAAAAABAwAAeHB0AAFBdAABQnQAAUNzcgAsc2NhbGEuY29sbGVjdGlvbi5pbW11dGFibGUu
+        |TGlzdFNlcmlhbGl6ZUVuZCSKXGNb91MLbQIAAHhweA==
+      """.stripMargin.trim.decoded[ List[ String ] ] mustEqual List( "A", "B", "C" )
     }
 
   }
