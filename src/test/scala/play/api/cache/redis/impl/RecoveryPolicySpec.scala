87cb1f1fb88d866f3821180678c3104805192744
Karel Cemus <karel.cemus@gmail.com>
2016-05-05 18:39:46 +0200

    RecoveryPolicy specification

---
+++ src/test/scala/play/api/cache/redis/impl/RecoveryPolicySpec.scala
@@ -0,0 +1,86 @@
+package play.api.cache.redis.impl
+
+import scala.concurrent.Future
+import scala.reflect.ClassTag
+
+import play.api.cache.redis.exception._
+
+import org.specs2.matcher.Matcher
+import org.specs2.mutable.Specification
+
+/**
+  * <p>Behavior of recovery policy.</p>
+  */
+class RecoveryPolicySpec extends Specification {
+
+  val futureDefault = Future.successful { }
+
+  new PolicySpecs(
+    "LogAndFail",
+    new LogAndFailPolicy,
+    throw new IllegalStateException( "Default should not trigger" ),
+    new Expectation {
+      override def expects[ T <: Throwable : ClassTag ]: Matcher[ Any ] = throwA[ T ]
+    }
+  )
+
+
+  new PolicySpecs(
+    "LogAndDefault",
+    new LogAndDefaultPolicy,
+    futureDefault,
+    new Expectation {
+      override def expects[ T <: Throwable : ClassTag ]: Matcher[ Any ] = beEqualTo( futureDefault )
+    }
+  )
+
+
+  trait Expectation {
+    def expects[ T <: Throwable : ClassTag ]: Matcher[ Any ]
+  }
+
+
+  class PolicySpecs( name: String, policy: RecoveryPolicy, default: => Future[ Unit ], expectation: Expectation ) {
+
+    import expectation._
+
+    def rerun: Future[ Unit ] =
+      throw new IllegalStateException( "Rerun should not trigger" )
+
+    val reason =
+      new IllegalStateException( "Failure reason" )
+
+    s"$name policy" should "recover from" >> {
+
+      "TimeoutException with a key" in {
+        policy.recoverFrom( rerun, default, new TimeoutException( Some( "key" ), "GET" ) ) must expects[ TimeoutException ]
+      }
+
+      "TimeoutException without a key" in {
+        policy.recoverFrom( rerun, default, new TimeoutException( None, "GET" ) ) must expects[ TimeoutException ]
+      }
+
+      "SerializationException" in {
+        policy.recoverFrom( rerun, default, new SerializationException( "key", "Serialization failed", reason ) ) must expects[ SerializationException ]
+      }
+
+      "ExecutionFailedException with a key" in {
+        policy.recoverFrom( rerun, default, new ExecutionFailedException( Some( "key" ), "GET", reason ) ) must expects[ ExecutionFailedException ]
+      }
+
+      "ExecutionFailedException without a key" in {
+        policy.recoverFrom( rerun, default, new ExecutionFailedException( None, "GET", reason ) ) must expects[ ExecutionFailedException ]
+      }
+
+      "UnexpectedResponseException with a key" in {
+        policy.recoverFrom( rerun, default, new UnexpectedResponseException( Some( "key" ), "GET" ) ) must expects[ UnexpectedResponseException ]
+      }
+
+      "UnexpectedResponseException without a key" in {
+        policy.recoverFrom( rerun, default, new UnexpectedResponseException( None, "GET" ) ) must expects[ UnexpectedResponseException ]
+      }
+    }
+
+  }
+
+}
