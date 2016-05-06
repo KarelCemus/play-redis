0618846ca2c63984037960feb67224fdbce53bb5
Karel Cemus <karel.cemus@gmail.com>
2016-05-06 12:37:44 +0200

    Builders implements recovery policy

--- src/main/scala/play/api/cache/redis/impl/Implicits.scala
+++ src/main/scala/play/api/cache/redis/impl/Implicits.scala
@@ -3,6 +3,8 @@ package play.api.cache.redis.impl
 import scala.concurrent.{ExecutionContext, Future}
 import scala.language.{higherKinds, implicitConversions}
 
+import play.api.cache.redis.exception.RedisException
+
 /** Implicit helpers used within the redis cache implementation. These
   * handful tools simplifies code readability but has no major function.
   *
@@ -20,18 +22,21 @@ private[ impl ] trait Implicits {
 
     /** Transforms the promise into desired builder results, possibly recovers with provided default value */
     def recoverWithDefault[ Result[ X ] ]( default: => T )( implicit builder: Builders.ResultBuilder[ Result ], policy: RecoveryPolicy, context: ExecutionContext, timeout: akka.util.Timeout ): Result[ T ] =
-      builder.toResult( future )
+      builder.toResult( future, Future.successful( default ) )
 
     /** recovers from the execution but returns future, not Result */
     def recoverWithFuture( default: => Future[ T ] )( implicit policy: RecoveryPolicy, context: ExecutionContext ): Future[ T ] =
-      future
+      future recoverWith {
+        // recover from known exceptions
+        case failure: RedisException => policy.recoverFrom( future, default, failure )
+      }
   }
 
   /** helper function enabling us to recover from command execution */
   implicit class RecoveryUnitFuture( future: => Future[ Unit ] ) {
     /** Transforms the promise into desired builder results, possibly recovers with provided default value */
     def recoverWithUnit[ Result[ X ] ]( implicit builder: Builders.ResultBuilder[ Result ], policy: RecoveryPolicy, context: ExecutionContext, timeout: akka.util.Timeout ): Result[ Unit ] =
-      builder.toResult( future )
+      builder.toResult( future, Future.successful( Unit ) )
   }
 
 }
