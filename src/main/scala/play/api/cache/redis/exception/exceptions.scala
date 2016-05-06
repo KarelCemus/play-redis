6b17bf654f44d1bf5961018f10982f2c7b9a203a
Karel Cemus <karel.cemus@gmail.com>
2016-05-06 12:58:20 +0200

    Cleaned up exception construction

--- src/main/scala/play/api/cache/redis/exception/exceptions.scala
+++ src/main/scala/play/api/cache/redis/exception/exceptions.scala
@@ -14,25 +14,25 @@ sealed abstract class RedisException( message: String, cause: Throwable ) extend
   *
   * @author Karel Cemus
   */
-case class TimeoutException( cause: Throwable ) extends RedisException( "" )
+case class TimeoutException( cause: Throwable ) extends RedisException( "Command execution timed out", cause )
 
 /**
   * Command execution failed with exception
   *
   * @author Karel Cemus
   */
-case class ExecutionFailedException( key: Option[ String ], command: String, cause: Throwable ) extends RedisException( command, cause )
+case class ExecutionFailedException( key: Option[ String ], command: String, cause: Throwable ) extends RedisException( s"Execution of '$command'${ key.map( key => s" for key '$key'" ) getOrElse "" } failed", cause )
 
 /**
   * Request succeeded but returned unexpected value
   *
   * @author Karel Cemus
   */
-case class UnexpectedResponseException( key: Option[ String ], command: String ) extends RedisException( command )
+case class UnexpectedResponseException( key: Option[ String ], command: String ) extends RedisException( s"Command '$command'${ key.map( key => s" for key '$key'" ) getOrElse "" } returned unexpected response" )
 
 /**
   * Value serialization or deserialization failed.
   *
   * @author Karel Cemus
   */
-case class SerializationException( key: String, message: String, cause: Throwable ) extends RedisException( message )
+case class SerializationException( key: String, message: String, cause: Throwable ) extends RedisException( s"$message failed for $key", cause )
