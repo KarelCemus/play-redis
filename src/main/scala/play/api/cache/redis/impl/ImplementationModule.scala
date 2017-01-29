f41ffdf7f26c89d2ac3300cf3f26aa8695599dd5
Karel Cemus <karel.cemus@gmail.com>
2017-01-29 11:54:31 +0100

    Exposed components for compile-time DI for impl package

--- src/main/scala/play/api/cache/redis/impl/ImplementationModule.scala
+++ src/main/scala/play/api/cache/redis/impl/ImplementationModule.scala
@@ -1,9 +1,48 @@
 package play.api.cache.redis.impl
 
-import play.api.cache.redis.{CacheApi, CacheAsyncApi}
 import play.api.cache.redis.exception._
-import play.api.inject.Module
+import play.api.cache.redis.{CacheApi, CacheAsyncApi}
+import play.api.inject.{Binding, Module}
 import play.api.{Configuration, Environment}
+
+/**
+  * Dispatches policy configuration and declares methods
+  * to initialize proper policy. This is reusable implementation
+  * encapsulating all supported values in configuration and
+  * to ensure consistency if the keys are added or changed.
+  *
+  * @author Karel Cemus
+  */
+private[ impl ] trait PolicyResolver[ T ] {
+
+  /** reads the configuration and provides proper recovery policy binding */
+  def resolve( configuration: Configuration ) = {
+    configuration.getString( "play.cache.redis.recovery" ) match {
+      case Some( "log-and-fail" ) => logAndFail
+      case Some( "log-and-default" ) => logAndDefault
+      case Some( "log-condensed-and-fail" ) => logCondensedAndFail
+      case Some( "log-condensed-and-default" ) => logCondensedAndDefault
+      case Some( "custom" ) => custom
+      case Some( _ ) => invalidConfiguration( "Invalid value in 'play.cache.redis.recovery'. Accepted values are 'log-and-fail', 'log-and-default', and 'custom'." )
+      case None => invalidConfiguration( "Key 'play.cache.redis.recovery' is mandatory. Accepted values are 'log-and-fail', 'log-and-default', and 'custom'." )
+    }
+  }
+
+  /** creates the policy based on the PolicyResolver implementation */
+  protected def logAndFail: T
+
+  /** creates the policy based on the PolicyResolver implementation */
+  protected def logAndDefault: T
+
+  /** creates the policy based on the PolicyResolver implementation */
+  protected def logCondensedAndFail: T
+
+  /** creates the policy based on the PolicyResolver implementation */
+  protected def logCondensedAndDefault: T
+
+  /** creates the policy based on the PolicyResolver implementation */
+  protected def custom: T
+}
 
 /**
   * Configures low-level classes communicating with the redis server.
@@ -11,6 +50,14 @@ import play.api.{Configuration, Environment}
   * @author Karel Cemus
   */
 object ImplementationModule extends Module {
+
+  private object RedisRecoveryPolicyResolver extends PolicyResolver[ Option[ Binding[ RecoveryPolicy ] ] ] {
+    protected def logAndFail = Some( bind[ RecoveryPolicy ].to[ LogAndFailPolicy ] )
+    protected def logAndDefault = Some( bind[ RecoveryPolicy ].to[ LogAndDefaultPolicy ] )
+    protected def logCondensedAndFail = Some( bind[ RecoveryPolicy ].to[ LogCondensedAndFailPolicy ] )
+    protected def logCondensedAndDefault = Some( bind[ RecoveryPolicy ].to[ LogCondensedAndDefaultPolicy ] )
+    protected def custom = None // do nothing, user provides own implementation binding
+  }
 
   override def bindings( environment: Environment, configuration: Configuration ) = Seq(
     // default binding for Play's CacheApi to SyncCache to replace default EHCache
@@ -21,18 +68,45 @@ object ImplementationModule extends Module {
     bind[ CacheAsyncApi ].to[ AsyncRedis ],
     // java api
     bind[ play.cache.CacheApi ].to[ JavaRedis ]
-  ) ++ policy( configuration )
+  ) ++ RedisRecoveryPolicyResolver.resolve( configuration )
+}
 
-  /** reads the configuration and provides proper recovery policy binding */
-  private def policy( configuration: Configuration ) = {
-    configuration.getString( "play.cache.redis.recovery" ) match {
-      case Some( "log-and-fail" ) => Some( bind[ RecoveryPolicy ].to[ LogAndFailPolicy ] )
-      case Some( "log-and-default" ) => Some( bind[ RecoveryPolicy ].to[ LogAndDefaultPolicy ] )
-      case Some( "log-condensed-and-fail" ) => Some( bind[ RecoveryPolicy ].to[ LogCondensedAndFailPolicy ] )
-      case Some( "log-condensed-and-default" ) => Some( bind[ RecoveryPolicy ].to[ LogCondensedAndDefaultPolicy ] )
-      case Some( "custom" ) => None // do nothing, user provides own implementation
-      case Some( _ ) => invalidConfiguration( "Invalid value in 'play.cache.redis.recovery'. Accepted values are 'log-and-fail', 'log-and-default', and 'custom'." )
-      case None => invalidConfiguration( "Key 'play.cache.redis.recovery' is mandatory. Accepted values are 'log-and-fail', 'log-and-default', and 'custom'." )
-    }
+/**
+  * Components for compile-time dependency injection.
+  * It binds components from impl package
+  *
+  * @author Karel Cemus
+  */
+private[ redis ] trait ImplementationComponents {
+
+  import play.api.cache.redis._
+
+  def configuration: Configuration
+
+  def environment: Environment
+
+  def redisConnector: RedisConnector
+
+  private object RecoveryPolicyResolver extends PolicyResolver[ RecoveryPolicy ] {
+    protected def logAndFail = new LogAndFailPolicy
+    protected def logAndDefault = new LogAndDefaultPolicy
+    protected def logCondensedAndFail = new LogCondensedAndFailPolicy
+    protected def logCondensedAndDefault = new LogCondensedAndDefaultPolicy
+    protected def custom = customRedisRecoveryPolicy // enable escape and overriding
   }
+
+  private lazy val policy: RecoveryPolicy = RecoveryPolicyResolver.resolve( configuration )
+
+  private lazy val syncRedis: SyncRedis = new SyncRedis( redisConnector, policy )
+
+  lazy val syncRedisCacheApi: CacheApi = syncRedis
+
+  lazy val asyncRedisCacheApi: CacheAsyncApi = new AsyncRedis( redisConnector, policy )
+
+  lazy val defaultCacheApi: play.api.cache.CacheApi = syncRedis
+
+  lazy val javaCacheApi: play.cache.CacheApi = new JavaRedis( syncRedis, environment )
+
+  protected def customRedisRecoveryPolicy: RecoveryPolicy =
+    shouldBeOverwritten( "In order to use custom RecoveryPolicy overwrite this method." )
 }
