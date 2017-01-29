01c841d153bf764d7b4a5deed6c493c902ae8af6
Karel Cemus <karel.cemus@gmail.com>
2017-01-29 12:01:16 +0100

    Specification using manually constructed redis cache api

---
+++ src/test/scala/play/api/cache/redis/RedisComponentsSpecs.scala
@@ -0,0 +1,43 @@
+package play.api.cache.redis
+
+import play.api._
+import play.api.inject.ApplicationLifecycle
+
+import org.specs2.mutable.Specification
+
+/**
+  * @author Karel Cemus
+  */
+class RedisComponentsSpecs extends Specification with Redis {
+
+  val components = new RedisCacheComponents {
+    def actorSystem = system
+    def applicationLifecycle = injector.instanceOf[ ApplicationLifecycle ]
+    def environment = injector.instanceOf[ Environment ]
+    def configuration = injector.instanceOf[ Configuration ]
+  }
+
+  private type Cache = CacheApi
+
+  private val Cache = components.syncRedisCacheApi
+
+  val prefix = "components-sync"
+
+  "SynchronousCacheApi" should {
+
+    "miss on get" in {
+      Cache.get[ String ]( s"$prefix-test-1" ) must beNone
+    }
+
+    "hit after set" in {
+      Cache.set( s"$prefix-test-2", "value" )
+      Cache.get[ String ]( s"$prefix-test-2" ) must beSome[ Any ]
+      Cache.get[ String ]( s"$prefix-test-2" ) must beSome( "value" )
+    }
+
+    "positive exists on existing keys" in {
+      Cache.set( s"$prefix-test-11", "value" )
+      Cache.exists( s"$prefix-test-11" ) must beTrue
+    }
+  }
+}
