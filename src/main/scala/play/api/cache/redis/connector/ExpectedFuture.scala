d65780c445d50e3240dc50876dd93153eaf7413b
Karel Cemus <karel.cemus@gmail.com>
2017-07-10 13:11:34 +0200

    Redesigned ExpectedFuture to properly identify key and command

--- src/main/scala/play/api/cache/redis/connector/ExpectedFuture.scala
+++ src/main/scala/play/api/cache/redis/connector/ExpectedFuture.scala
@@ -10,32 +10,64 @@ import play.api.cache.redis.exception._
   *
   * @author Karel Cemus
   */
-private[ connector ] class ExpectedFuture[ T ]( future: Future[ T ], key: String, cmd: => String ) {
+private[ connector ] trait ExpectedFuture[ T ] {
+
+  protected def future: Future[ T ]
+
+  protected def cmd: String
 
   /** received an unexpected response */
-  private def onUnexpected( key: Option[ String ], cmd: => String ): PartialFunction[ Any, Nothing ] = {
-    case _ => unexpected( key, cmd )
-  }
+  protected def onUnexpected: PartialFunction[ Any, Nothing ]
+
+  protected def onFailed( ex: Throwable ): Nothing
 
   /** execution failed with an exception */
-  private def onException( key: Option[ String ], cmd: => String ): PartialFunction[ Throwable, Nothing ] = {
-    case ex => failed( key, cmd, ex )
+  private def onException: PartialFunction[ Throwable, Nothing ] = {
+    case ex => onFailed( ex )
   }
 
   /** handles both expected and unexpected responses and failure recovery */
   def expects[ U ]( expected: PartialFunction[ T, U ] )( implicit context: ExecutionContext ): Future[ U ] = {
-    future map ( expected orElse onUnexpected( Some( key ), cmd ) ) recover onException( Some( key ), cmd )
+    future map ( expected orElse onUnexpected ) recover onException
+  }
+}
+
+private[ connector ] class ExpectedFutureWithoutKey[ T ]( protected val future: Future[ T ], protected val cmd: String ) extends ExpectedFuture[ T ] {
+
+  protected def onUnexpected: PartialFunction[ Any, Nothing ] = {
+    case _ => unexpected( None, cmd )
   }
 
-  def withParameter( param: String ) = withParameters( param )
+  protected def onFailed( ex: Throwable ): Nothing =
+    failed( None, cmd, ex )
 
-  def withParameters( params: String ) = new ExpectedFuture( future, key, s"$key $params" )
+  def withKey( key: String ): ExpectedFutureWithKey[ T ] = new ExpectedFutureWithKey[ T ]( future, cmd, key, s"$cmd $key" )
+
+  def withKeys( keys: Traversable[ String ] ): ExpectedFutureWithKey[ T ] = withKey( keys mkString " " )
+}
+
+private[ connector ] class ExpectedFutureWithKey[ T ]( protected val future: Future[ T ], protected val cmd: String, key: String, fullCommand: => String ) extends ExpectedFuture[ T ] {
+
+  protected def onUnexpected: PartialFunction[ Any, Nothing ] = {
+    case _ => unexpected( Some( key ), cmd )
+  }
+
+  protected def onFailed( ex: Throwable ): Nothing =
+    failed( Some( key ), cmd, ex )
+
+  def andParameter( param: => Any ): ExpectedFutureWithKey[ T ] = andParameters( param.toString )
+
+  def andParameters( params: Traversable[ Any ] ): ExpectedFutureWithKey[ T ] = andParameters( params mkString " " )
+
+  def andParameters( params: => String ): ExpectedFutureWithKey[ T ] = new ExpectedFutureWithKey( future, cmd, key, s"$fullCommand $params" )
+
+  def asCommand( commandOverride: => String ) = new ExpectedFutureWithKey( future, cmd, key, s"$cmd $commandOverride" )
 }
 
 /**
   * Constructs expected future from provided parameters, this serves as syntax sugar
   */
-private[ connector ] class ExpectedFutureBuilder[ T ]( future: Future[ T ] ) {
+private[ connector ] class ExpectedFutureBuilder[ T ]( val future: Future[ T ] ) extends AnyVal {
 
-  def executing( key: String ): ExpectedFuture[ T ] = new ExpectedFuture[ T ]( future, key, key )
+  def executing( cmd: String ): ExpectedFutureWithoutKey[ T ] = new ExpectedFutureWithoutKey[ T ]( future, cmd )
 }
