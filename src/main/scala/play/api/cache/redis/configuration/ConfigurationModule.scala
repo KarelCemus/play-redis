ec3d357718c2bb4dcc921a115520e0522a5fd0dd
Karel Cemus <karel.cemus@gmail.com>
2017-07-31 11:26:37 +0200

    Redesigned play-redis configuration

--- src/main/scala/play/api/cache/redis/configuration/ConfigurationModule.scala
+++ src/main/scala/play/api/cache/redis/configuration/ConfigurationModule.scala
@@ -1,95 +1,19 @@
 package play.api.cache.redis.configuration
 
-import play.api.cache.redis.exception._
-import play.api.inject.{Binding, Module}
-import play.api.{Configuration, Environment}
+import play.api.Configuration
+import play.api.inject._
 
 /**
-  * Dispatches redis url configuration and declares methods
-  * to initialize the configuration provider properly.
-  * This is reusable implementation encapsulating all supported values
-  * in configuration and to ensure consistency if the keys are
-  * added or changed.
+  * Extracts the configuration and binds with DI for upper layers.
   *
   * @author Karel Cemus
   */
-private[ configuration ] trait ConfigurationResolver[ T ] {
+object ConfigurationModule extends SimpleModule( ( environment, configuration ) => {
 
-  /** returns configuration provider based on the application configuration */
-  def resolve( configuration: Configuration ) = configuration.get[ String ]( "play.cache.redis.configuration" ) match {
-    // required static implementation using application.conf
-    case "static" => static
-    // required environmental implementation
-    case "env" => env( connectionStringVariable( configuration ) )
-    // required heroku configuration
-    case "heroku" => heroku
-    // required heroku configuration
-    case "heroku-cloud" => herokuCloud
-    // supplied custom implementation
-    case "custom" => custom
-    // found but unrecognized
-    case other => invalidConfiguration( s"Unrecognized configuration provider '$other' in key 'play.cache.redis.configuration'. Expected values are 'custom', 'static', 'heroku', 'heroku-cloud', and 'env'." )
+  configuration.get( "play.cache.redis" )( RedisInstanceManager ).foldLeft( List.empty[ Binding[ RedisInstance ] ] ) {
+    ( binding, binder ) => binder.toBinding ::: binding
   }
-
-  /** returns name of the variable with the connection string */
-  private def connectionStringVariable( configuration: Configuration ) =
-    configuration.get[ String ]( "play.cache.redis.connection-string-variable" )
-
-  /**
-    * creates the configuration provider based on the resolver implementation
-    *
-    * Note: required static implementation using application.conf
-    */
-  protected def static: T
-
-  /**
-    * creates the configuration provider based on the resolver implementation
-    *
-    * Note: required environmental implementation
-    */
-  protected def env( connectionString: String ): T
-
-  /**
-    * creates the configuration provider based on the resolver implementation
-    *
-    * Note: required heroku configuration
-    */
-  protected def heroku: T
-
-  /**
-    * creates the configuration provider based on the resolver implementation
-    *
-    * Note: required heroku configuration
-    */
-  protected def herokuCloud: T
-
-  /**
-    * creates the configuration provider based on the resolver implementation
-    *
-    * Note: supplied custom implementation
-    */
-  protected def custom: T
-}
-
-
-/**
-  * Configures low-level classes communicating with the redis server.
-  *
-  * @author Karel Cemus
-  */
-object ConfigurationModule extends Module {
-
-  private object RedisConfigurationResolver extends ConfigurationResolver[ Option[ Binding[ RedisConfiguration ] ] ] {
-    protected def static = Some( bind[ RedisConfiguration ].to[ ConfigurationFile ] )
-    protected def env( connectionString: String ) = Some( bind[ RedisConfiguration ].to( new ConnectionStringProvider( connectionString ) ) )
-    protected def heroku = Some( bind[ RedisConfiguration ].to( new ConnectionStringProvider( "REDIS_URL" ) ) )
-    protected def herokuCloud = Some( bind[ RedisConfiguration ].to( new ConnectionStringProvider( "REDISCLOUD_URL" ) ) )
-    protected def custom = None // ignore, supplied custom configuration provider
-  }
-
-  override def bindings( environment: Environment, configuration: Configuration ) =
-    RedisConfigurationResolver.resolve( configuration ).toSeq
-}
+} )
 
 
 /**
@@ -102,16 +26,18 @@ private[ redis ] trait ConfigurationComponents {
 
   def configuration: Configuration
 
-  private object RedisConfigurationResolver extends ConfigurationResolver[ RedisConfiguration ] {
-    protected def static = new ConfigurationFile
-    protected def env( connectionString: String ) = new ConnectionStringProvider( connectionString ).get()
-    protected def heroku = new ConnectionStringProvider( "REDIS_URL" ).get()
-    protected def herokuCloud = new ConnectionStringProvider( "REDISCLOUD_URL" ).get()
-    protected def custom = customRedisConfiguration
-  }
+  /** creates a redis manager from the configuration */
+  final def newRedisCacheManager = configuration.get( "play.cache.redis" )( RedisInstanceManager )
 
-  protected def customRedisConfiguration: RedisConfiguration =
-    shouldBeOverwritten( "In order to use custom RedisConfiguration overwrite this method." )
-
-  lazy val redisConfiguration = RedisConfigurationResolver.resolve( configuration )
+  /**
+    *
+    * To provide custom cache configuration override this variable,
+    * define custom RedisInstanceManager and fold it with the newRedisCacheManager
+    * creating the original configuration. Join managers by `++` operation.
+    *
+    * `
+    *   override lazy val redisCacheManager = newRedisCacheManager ++ new RedisInstanceManager( Map.empty )
+    * `
+    */
+  lazy val redisCacheManager = newRedisCacheManager
 }
---
+++ src/main/scala/play/api/cache/redis/configuration/RedisHost.scala
@@ -0,0 +1,88 @@
+package play.api.cache.redis.configuration
+
+import play.api.ConfigLoader
+
+import com.typesafe.config.Config
+
+/**
+  * Configures a single node either a standalone or within a cluster.
+  *
+  * @author Karel Cemus
+  */
+trait RedisHost {
+  /** host with redis server */
+  def host: String
+  /** port redis listens on */
+  def port: Int
+  /** redis database identifier to work with */
+  def database: Option[ Int ]
+  /** when enabled security, this returns password for the AUTH command */
+  def password: Option[ String ]
+  /** trait-specific equals */
+  override def equals( obj: scala.Any ) = equalsAsHost( obj )
+  /** trait-specific equals, invokable from children */
+  protected def equalsAsHost( obj: scala.Any ) = obj match {
+    case that: RedisHost => this.host == that.host && this.port == that.port && this.database == that.database && this.password == that.password
+    case _ => false
+  }
+  /** to string */
+  override def toString = (password, database) match {
+    case ( Some( password ), Some( database ) ) => s"redis://redis:$password@$host:$port?db=$database"
+    case ( Some( password ), None ) => s"redis://redis:$password@$host:$port"
+    case ( None, Some( database ) ) => s"redis://$host:$port?db=$database"
+    case ( None, None ) => s"redis://$host:$port"
+  }
+}
+
+
+object RedisHost extends ConfigLoader[ RedisHost ] {
+  import RedisConfigLoader._
+
+  /** expected format of the environment variable */
+  private val ConnectionString = "redis://((?<user>[^:]+):(?<password>[^@]+)@)?(?<host>[^:]+):(?<port>[0-9]+)".r( "auth", "user", "password", "host", "port" )
+
+  def load( config: Config, path: String ): RedisHost = apply(
+    host = config.getString( path / "host" ),
+    port = config.getInt( path / "port" ),
+    database = config.getOption( path / "database", _.getInt ),
+    password = config.getOption( path / "password", _.getString )
+  )
+
+  /** read environment url or throw an exception */
+  def fromConnectionString( connectionString: String ): RedisHost = ConnectionString findFirstMatchIn connectionString match {
+    // read the environment variable and fill missing information from the local configuration file
+    case Some( matcher ) => new RedisHost {
+      val host = matcher.group( "host" )
+      val port = matcher.group( "port" ).toInt
+      val database = None
+      val password = Option( matcher.group( "password" ) )
+    }
+    // unexpected format
+    case None => throw new IllegalArgumentException( s"Unexpected format of the connection string: '$connectionString'. Expected format is 'redis://[user:password@]host:port'." )
+  }
+
+  def apply( host: String, port: Int, database: Option[ Int ] = None, password: Option[ String ] = None ): RedisHost =
+    create( host, port, database, password )
+
+  /** hackish method to preserve nice names of parameters in apply */
+  @inline private def create( _host: String, _port: Int, _database: Option[ Int ], _password: Option[ String ] ) = new RedisHost {
+    val host = _host
+    val port = _port
+    val database = _database
+    val password = _password
+  }
+}
+
+/**
+  *
+  * A helper trait delegating properties into the inner settings object
+  *
+  * @author Karel Cemus
+  */
+trait RedisDelegatingHost extends RedisHost {
+  def innerHost: RedisHost
+  def host = innerHost.host
+  def port = innerHost.port
+  def database = innerHost.database
+  def password = innerHost.password
+}
