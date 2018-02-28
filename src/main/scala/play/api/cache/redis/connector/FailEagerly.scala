234fc68907b322e82f37d8d51ae862965b1bc4f9
Karel Cemus <karel.cemus@gmail.com>
2018-02-28 15:30:44 +0100

    Alternative eager failure implementation
    The original too eager solution caused issues
    on Travis CI and possibly elsewhere

--- src/main/scala/play/api/cache/redis/connector/FailEagerly.scala
+++ src/main/scala/play/api/cache/redis/connector/FailEagerly.scala
@@ -1,9 +1,11 @@
 package play.api.cache.redis.connector
 
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
+import akka.actor._
+import akka.pattern._
 import redis._
-
 
 /**
   * Actor extension maintaining current connected status.
@@ -12,13 +14,24 @@ import redis._
   * instead.
   *
   */
-trait FailEagerly extends redis.Request {
+trait FailEagerly extends Request {
 
   protected var connected = false
 
+  protected val scheduler: Scheduler
+
+  /** max timeout of the future when the redis is disconnected */
+  @inline
+  private val failAfter = 300.millis
+
   abstract override def send[ T ]( redisCommand: RedisCommand[ _ <: protocol.RedisReply, T ] ) = {
+    // proceed with the command
+    @inline def continue = super.send( redisCommand )
     // fails eagerly when the connection is not established
-    if ( connected ) super.send( redisCommand )
-    else Future.failed( redis.actors.NoConnectionException )
+    @inline def fail = after( failAfter, scheduler )( Future.failed( redis.actors.NoConnectionException ) )
+    // first completed
+    @inline def orFail = Future.firstCompletedOf( Seq( continue, fail ) )
+    // based on connection status
+    if ( connected ) continue else orFail
   }
 }
