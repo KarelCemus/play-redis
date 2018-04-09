fae7de34a336e2963de127d77cdf884104016d05
Karel Cemus <karel.cemus@gmail.com>
2018-04-09 13:51:09 +0200

    Configuration tests

---
+++ src/test/scala/play/api/cache/redis/configuration/RedisHostSpecs.scala
@@ -0,0 +1,42 @@
+package play.api.cache.redis.configuration
+
+import org.specs2.mutable.Spec
+
+/**
+  * @author Karel Cemus
+  */
+class RedisHostSpecs extends Spec {
+
+  private implicit val loader = RedisHost
+
+  "host with database and password" in new WithConfiguration(
+    """
+      |play.cache.redis {
+      |  host:     localhost
+      |  port:     6378
+      |  database: 1
+      |  password: something
+      |}
+    """
+  ) {
+    configuration.get[ RedisHost ]( "play.cache.redis" ) mustEqual RedisHost( "localhost", 6378, database = 1, password = "something" )
+  }
+
+  "host without database and password" in new WithConfiguration(
+    """
+      |play.cache.redis {
+      |  host:     localhost
+      |  port:     6378
+      |}
+    """
+  ) {
+    configuration.get[ RedisHost ]( "play.cache.redis" ) mustEqual RedisHost( "localhost", 6378, database = 0 )
+  }
+
+  "host from connection string" in {
+    RedisHost.fromConnectionString( "redis://redis:something@localhost:6378" ) mustEqual RedisHost( "localhost", 6378, password = "something" )
+    RedisHost.fromConnectionString( "redis://localhost:6378" ) mustEqual RedisHost( "localhost", 6378 )
+    // test invalid string
+    RedisHost.fromConnectionString( "redis:/localhost:6378" ) must throwA[ IllegalArgumentException ]
+  }
+}
