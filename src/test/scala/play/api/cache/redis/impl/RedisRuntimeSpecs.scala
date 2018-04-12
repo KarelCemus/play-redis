33c1955d4826fd192a42acb28d26d1770daa09fb
Karel Cemus <karel.cemus@gmail.com>
2018-04-12 12:33:23 +0200

    RedisRuntime creation specification

---
+++ src/test/scala/play/api/cache/redis/impl/RedisRuntimeSpecs.scala
@@ -0,0 +1,55 @@
+package play.api.cache.redis.impl
+
+import play.api.cache.redis._
+
+import org.specs2.mutable.Specification
+
+/**
+  * @author Karel Cemus
+  */
+class RedisRuntimeSpecs extends Specification with WithApplication {
+
+  import Implicits._
+
+  implicit val recoveryResolver = new RecoveryPolicyResolverImpl
+
+  "RedisRuntime" should {
+    import RedisRuntime._
+
+    "be build from config (A)" in {
+      val runtime = RedisRuntime(
+        instance = defaultInstance,
+        recovery = "log-and-fail",
+        invocation = "eager",
+        prefix = None
+      )
+
+      runtime.timeout mustEqual akka.util.Timeout( defaultInstance.timeout.sync )
+      runtime.policy must beAnInstanceOf[ LogAndFailPolicy ]
+      runtime.invocation mustEqual EagerInvocation
+      runtime.prefix mustEqual RedisEmptyPrefix
+    }
+
+    "be build from config (B)" in {
+      val runtime = RedisRuntime(
+        instance = defaultInstance,
+        recovery = "log-and-default",
+        invocation = "lazy",
+        prefix = Some( "prefix" )
+      )
+
+      runtime.policy must beAnInstanceOf[ LogAndDefaultPolicy ]
+      runtime.invocation mustEqual LazyInvocation
+      runtime.prefix mustEqual new RedisPrefixImpl( "prefix" )
+    }
+
+    "be build from config (C)" in {
+      RedisRuntime(
+        instance = defaultInstance,
+        recovery = "log-and-default",
+        invocation = "other",
+        prefix = Some( "prefix" )
+      ) must throwA[ IllegalArgumentException ]
+    }
+  }
+}
