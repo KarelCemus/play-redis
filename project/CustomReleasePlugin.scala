58951f43c6ac59877658f49faa2b107be5a033d3
Karel Cemus <karel.cemus@gmail.com>
2018-05-08 16:02:02 +0200

    Moved Maven configuration settings into CustomReleasePlugin

--- project/CustomReleasePlugin.scala
+++ project/CustomReleasePlugin.scala
@@ -1,3 +1,4 @@
+import sbt.Keys._
 import sbt._
 import sbtrelease._
 
@@ -14,14 +15,17 @@ object CustomReleasePlugin extends AutoPlugin {
     val playVersion = settingKey[ String ]( "Version of Play framework" )
     val connectorVersion = settingKey[ String ]( "Version redis connector" )
     val specs2Version = settingKey[ String ]( "Version of Specs2 testing framework" )
+
+    val authors = settingKey[ Seq[ String ] ]( "List of authors of the library" )
+    val vcsScm = settingKey[ String ]( "URL of the GIT repository" )
   }
 
   override def trigger = allRequirements
 
   override def requires = ReleasePlugin
 
-  override def projectSettings = Seq[ Setting[ _ ] ](
-    releaseProcess := Seq[ ReleaseStep ](
+  private def customizedReleaseProcess = {
+    Seq[ ReleaseStep ](
       checkSnapshotDependencies,
       inquireVersions,
       runClean,
@@ -36,5 +40,29 @@ object CustomReleasePlugin extends AutoPlugin {
       commitNextVersion,
       pushChanges
     )
+  }
+
+  import autoImport._
+
+  override def projectSettings = Seq[ Setting[ _ ] ](
+    publishMavenStyle := true,
+    pomIncludeRepository := { _ => false},
+    pomExtra := {
+      <scm>
+        <url>{vcsScm.value}</url>
+        <connection>scm:{vcsScm.value}</connection>
+      </scm>
+      <developers>
+        {
+          authors.value.map { author =>
+            <developer>
+              <name>{author}</name>
+            </developer>
+          }
+        }
+      </developers>
+    },
+    // customized release process
+    releaseProcess := customizedReleaseProcess
   )
 }
