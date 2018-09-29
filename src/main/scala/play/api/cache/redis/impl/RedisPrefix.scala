392952b96c22572b97acba10baf05aa0469e767e
Karel Cemus <karel.cemus@gmail.com>
2018-09-29 13:51:20 +0200

    Matching operation unprefixes keys when prefix is defined

--- src/main/scala/play/api/cache/redis/impl/RedisPrefix.scala
+++ src/main/scala/play/api/cache/redis/impl/RedisPrefix.scala
@@ -6,15 +6,21 @@ package play.api.cache.redis.impl
   */
 sealed trait RedisPrefix extends Any {
   @inline def prefixed(key: String): String
+  @inline def unprefixed(key: String): String
   @inline def prefixed(key: Seq[String]): Seq[String]
+  @inline def unprefixed(key: Seq[String]): Seq[String]
 }
 
 class RedisPrefixImpl(val prefix: String) extends AnyVal with RedisPrefix {
   @inline def prefixed(key: String) = s"$prefix:$key"
+  @inline def unprefixed(key: String) = key.drop(prefix.length + 1)
   @inline def prefixed(keys: Seq[String]) = keys.map(prefixed)
+  @inline def unprefixed(keys: Seq[String]) = keys.map(unprefixed)
 }
 
 object RedisEmptyPrefix extends RedisPrefix {
   @inline def prefixed(key: String) = key
+  @inline def unprefixed(key: String) = key
   @inline def prefixed(keys: Seq[String]) = keys
+  @inline def unprefixed(keys: Seq[String]) = keys
 }
